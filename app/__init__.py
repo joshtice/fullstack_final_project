23d51b661e634e72450252e4eedf6f1aab0a7d00
Joshua Tice <joshtice@gmail.com>
2020-04-18 14:16:18 -0500

    Adjust configurations to correctly run on Heroku  On branch master
     Changes to be committed:
    	modified:   app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,22 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__, instance_relative_config=True)
 
 # Load default configuration
 app.config.from_object('config.default')
-# Load instance variables, including secrets
-app.config.from_pyfile('config.py')
-# Load configurations specified by environment variable
-app.config.from_envvar('APP_CONFIG_FILE')
+
+# Determine whether running development or production, and load 
+# environment variables appropriately
+app.config.from_envvar('APP_MODE')
+if app.config['APP_MODE'] == 'development':
+    app.config.from_object('config.developmenty')
+    app.config.from_pyfile('instance_config.py')
+elif app.config['APP_MODE' == 'production':
+    app.config.from_object('config.production')
+    app.config.from_envvar('SECRET_KEY')
+    app.config.from_envvar('DATABASE_URL')
+
 
 from app import views
