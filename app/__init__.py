554581f97da005fd2d0c85ea3c03f5a874378226
Joshua Tice <joshtice@gmail.com>
2020-04-19 14:35:17 -0500

    Add initial database models  On branch development 	modified:   app/__init__.py 	modified:   app/models.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 import os
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__, instance_relative_config=True)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Load default configuration
 app.config.from_object('config.default')
---
+++ app/models.py
@@ -0,0 +1,95 @@
+from app import db
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(32))
+    last_name = db.Column(db.String(32))
+    department = db.Column(db.String(32))
+    errors = db.relationship('Error', backref='user', lazy=True)
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return ( 
+            {
+                'id': self.id,
+                'first_name': self.first_name,
+                'last_name': self.last_name,
+                'department': self.department,
+            }
+        )
+
+
+class Instrument(db.Model):
+    __tablename__ = 'instrument'
+
+    id = db.Column(db.Integer, primary_key=True)
+    serial_number = db.Column(db.String(32))
+    ip_address = db.Column(db.String(32))
+    errors = db.relationshipo('Error', backref='instrument', lazy=True)
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return (
+            {
+                'id': self.id,
+                'serial_number': self.serial_number,
+                'ip_address': self.ip_address,
+            }
+        )
+
+
+class Error(db.Model):
+    __tablename__ = 'error'
+
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(256))
+    is_resolved = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')
+        nullable=False)
+    instrument_id = db.Column(db.Integer, db.ForeignKey('instrument.id'),
+        nullable=False)
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return (
+            {
+                'id': self.id,
+                'description': self.description,
+                'is_resolved': self.is_resolved,
+                'user': self.user.format(),
+                'instrument': self.instrument.format(),
+            }
+        )
