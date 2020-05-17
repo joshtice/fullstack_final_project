2692c260bb6302f13a71569fa59c9037b50545c0
Joshua Tice <joshtice@gmail.com>
2020-05-16 19:02:37 -0500

    Add app factory and modify test_app.py to successfully run non-trivial tests
    On branch development
    Changes to be committed:
    	modified:   app/__init__.py
    	modified:   tests/test_app.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,26 +4,32 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-app = Flask(__name__, instance_relative_config=True)
+db = SQLAlchemy()
+migrate = Migrate()
 
-# Load default configuration
-app.config.from_object('config.default')
+def create_app():
 
-# Determine whether running development or production, and load 
-# environment variables appropriately
-app.config['APP_MODE'] = os.environ.get('APP_MODE')
-if app.config['APP_MODE'] == 'development':
-    app.config.from_object('config.development')
-    app.config.from_object('instance.config')
-elif app.config['APP_MODE'] == 'test':
-    app.config.from_object('config.test')
-elif app.config['APP_MODE'] == 'production':
-    app.config.from_object('config.production')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app = Flask(__name__, instance_relative_config=True)
 
+    # Load default configuration
+    app.config.from_object('config.default')
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+    # Determine whether running development or production, and load 
+    # environment variables appropriately
+    app.config['APP_MODE'] = os.environ.get('APP_MODE')
+    if app.config['APP_MODE'] == 'development':
+        app.config.from_object('config.development')
+        app.config.from_object('instance.config')
+    elif app.config['APP_MODE'] == 'test':
+        app.config.from_object('config.test')
+    elif app.config['APP_MODE'] == 'production':
+        app.config.from_object('config.production')
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
+    db.init_app(app)
+    migrate.init_app(app)
 
+    return app
+
+app = create_app()
 from app import models, auth, views
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,19 +1,20 @@
 from datetime import datetime
 import json
-import base64
 import os
-import subprocess
-import unittest
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from jose import jwt
+import subprocess
 
-from app import app, db
+from app import create_app
 from app.models import Contact, Instrument, Error
 
 from pprint import pprint
 
 import pytest
-import tempfile
+
+
+from app import *
 
 
 @pytest.fixture
@@ -21,56 +22,42 @@ def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         with app.app_context():
-            app.init_db()
+            db.create_all()
         yield client
 
+@pytest.fixture
+def admin_auth():
+    yield {'Authorization': f"Bearer {os.environ.get('ADMIN_TOKEN')}"}
 
 
-def test_database_uri():
-    subprocess.run(['psql', '-f', f'{os.getcwd()}/tests/setup_test_db.sql'])
-    assert app.config['SQLALCHEMY_DATABASE_URI'] == 'postgres://app_test_user@localhost:5432/error_logging_app_test'
-    subprocess.run(['psql', '-f', f'{os.getcwd()}/tests/setup_test_db.sql'])
+def test_database_setup():
+    assert app.config['SQLALCHEMY_DATABASE_URI'] == (
+        'postgres://app_test_user@localhost:5432/error_logging_app_test')
 
-#     """
-#     Test the authorization, CRUD operations, and error handlers for app
-#     """
+def test_client_status(client):
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert json.loads(response.data) == {'status': 'healthy'}
 
-#     def setUp(self):
-#         self.app = app.test_client()
-#         db.create_all()
-#         contacts = [
-#             Contact('jane', 'doe', 'system integration'),
-#             Contact('john', 'doe', 'assay development'),
-#         ]
-#         instruments = [
-#             Instrument('VRGN001', '127.0.0.24'),
-#             Instrument('VRGN002', '127.0.0.25'),
-#         ]
-#         errors = [
-#             Error('software freeze', contacts[0], instruments[0]),
-#             Error('broken bay', contacts[1], instruments[1]),
-#         ]
-#         [contact.insert() for contact in contacts]
-#         [instrument.insert() for instrument in instruments]
-#         [error.insert() for error in errors]
+@pytest.mark.skip()
+def test_contacts_post(client, admin_auth_header):
+    data = json.dumps({
+        'first_name': 'contact',
+        'last_name': '1',
+        'department': 'department 1',
+    })
+    response = client.post(
+        '/contacts', data=data, headers=admin_auth_header)
+    assert response.status_code == 200
+    # assert json.loads(response.data) == data
 
-#     def tearDown(self):
-#         db.session.close()
-#         db.session.remove()
-#         db.drop_all()
+def test_contacts_get(client, admin_auth):
+    response=client.get('/contacts', headers=admin_auth)
+    assert response.status_code == 200
 
-#     def test_client(self):
-#         response = self.app.get('/')
-#         data = json.loads(response.data)
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(data['status'], 'healthy')
+def test_instruments_get(client, admin_auth):
+    response=client.get('/instruments', headers=admin_auth)
+    assert response.status_code == 200
 
-#     def test_get_contacts(self):
-#         response = self.app.get('/contacts')
-#         # self.assertEqual(response.status_code, 200)
-
-# if __name__ == '__main__':
-#     subprocess.run(['psql', '-f', f'{os.getcwd()}/tests/setup_test_db.sql'])
-#     unittest.main(exit=False)
-#     pprint(locals().keys())
-#     subprocess.run(['psql', '-f', f'{os.getcwd()}/tests/teardown_test_db.sql'])
+def test_contacts_delete(client):
+    pass
